693fcceef515eb8da59bddd4547a5a96d5b7c7f9
manius55 <m.biedrzycki@o2.pl>
2022-05-03 04:05:25 +0200

    display profile description and date of birth only if exists

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -27,14 +27,18 @@
                     @endif
                 </div>
             </div>
+            @if($profile->description !== null)
             <div class="row justify-content-center py-2">
                 <label for="description" class="text-center"><strong>Description:</strong></label>
                 <div id="description" class="border col-6 border border-dark rounded" style="height: 75px"> <strong>{{$profile->description}} </strong> </div>
             </div>
+            @endif
+            @if($profile->date_of_birth !== null)
             <div class="row justify-content-center">
                 <label for="date_of_birth" class="text-center"><strong>Date of birth</strong></label>
                 <div id="date_of_birth" class="col-2 py-2 border text-center border-dark rounded">{{$profile->date_of_birth}}</div>
             </div>
+            @endif
         </div>
     </div>
 @endsection
