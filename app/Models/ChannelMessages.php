b0e3d0d427a01119cbd8d7dbb5c3a9f1193b078d
manius55 <m.biedrzycki@o2.pl>
2022-05-04 02:01:27 +0200

    add soft deletes to channelMesasges,add ChannelAdmin model,migr

--- app/Models/ChannelMessages.php
+++ app/Models/ChannelMessages.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ChannelMessages extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         'channel_id',
         'from_user_id',
         'message'
     ];
-
     public function user()
     {
         return $this->belongsTo(User::class, 'from_user_id');
