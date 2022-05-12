729d351f006db79f648942d4cd5e004cca0c795c
manius55 <m.biedrzycki@o2.pl>
2022-05-12 03:03:17 +0200

    change pusher key

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,7 +26,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: '9bd017030ce620b4a380',
+    cluster: 'eu',
     forceTLS: true
 });
